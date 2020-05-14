7f07a33afc3831af449276428062eee43bdcc74c
roushansourav <roushan.sourav@settlin.in>
2020-05-15 03:25:02 +0530

    google-oAuth

--- index.js
+++ index.js
@@ -3,12 +3,25 @@
  * alias:VenomWolf
  */
 const express =require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 require('dotenv').config();
+require('./models/user');
 require('./services/passport');
 
+mongoose.connect(process.env.MONGO_URI);
 const app=express();
 const PORT=process.env.PORT || 5000;
 
+app.use(
+	cookieSession({
+		maxAge:30*24*60*60*1000,
+		keys:[process.env.cookie_KEYS]
+	})
+);
+app.use(passport.initialize());
+app.use(passport.session());
 app.get('/',function(req,res){
 	res.send('bye buddy')
 });
--- services/passport.js
+++ services/passport.js
@@ -1,5 +1,20 @@
 const passport =require('passport');
 const GoogleStrategy=require('passport-google-oauth20').Strategy;
+const mongoose=require('mongoose');
+
+const User=mongoose.model('users');
+
+passport.serializeUser((user,done)=>{
+	done(null,user.id);
+});
+
+passport.deserializeUser((id,done)=>{
+	User.findById(id)
+		.then(user=>{
+			done(null,user);
+		})
+		.catch(err=>done(err,null))
+})
 
 passport.use(new GoogleStrategy(
 	{
@@ -7,6 +22,20 @@ passport.use(new GoogleStrategy(
 		clientSecret:process.env.googleCLIENT_SECRET,
 		callbackURL:process.env.googleCALLBACK_URI,
 	},function(accessToken,refreshToken,profile,done){
-		console.log('accessToken: ',accessToken,'refreshToken: ',refreshToken,'profile: ',profile);
+		User.findOne({googleId:profile.id})
+			.then((user)=>{
+				if(user){
+					//we are having record.
+					//done(errmsg,userrecord);
+					done(null,user);
+				}
+				else{
+					//we don't have user record.
+					new User({googleId:profile.id}).save()
+						.then(user=>done(null,user))
+						.catch((err)=>done(err,null))
+				}
+			})
+			.catch((err)=>done(err,null))		
 	}
 ));
