bac8ba63c8619314f8fdcddf7bf943bc8e126e6b
roushansourav <roushan.sourav@settlin.in>
2020-05-14 20:18:37 +0530

    env var

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 const express =require('express');
+const passport =require('passport');
+const GoogleStrategy=require('passport-google-oauth20').Strategy;
+require('dotenv').config();
 
 const app=express();
 const PORT=process.env.PORT || 5000;
+passport.use(new GoogleStrategy(
+	{
+		clientID:process.env.googleCLIENT_ID,
+		clientSecret:process.env.googleCLIENT_SECRET,
+		callbackURL:process.env.googleCALLBACK_URI,
+	},function(accessToken){
+		console.log(accessToken)
+	}
+));
 
-app.get('/',(req,res)=>{
-	res.send({bye:'buddy'});
-});
+app.get('/auth/google',
+passport.authenticate('google',{
+	scope:['profile','email']
+}));
+
+
+
+//Server listening
 app.listen(PORT,()=>{
 	console.log('Server is Running')
 });
